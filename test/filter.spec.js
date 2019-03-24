e55692136712f4fd904ecb1e71262c903f35f7ca
buchslava <vyacheslav.chub@gmail.com>
2019-03-24 22:03:14 +0200

    tests for in and not in clauses

--- test/filter.spec.js
+++ test/filter.spec.js
@@ -22,7 +22,7 @@ describe('filters', () => {
 
     done();
   });
-  it.only('in operator', done => {
+  it('in operator', done => {
     const sqlQuery = `
     SELECT geo, time, income_mountains FROM datapoints 
     WHERE time=2018 AND (geo IN ('world', 'country') 
@@ -30,6 +30,48 @@ describe('filters', () => {
     const parser = new Parser();
     const ast = parser.parse(sqlQuery);
     const filterFun = getRecordFilterFun(sqlQuery, ast);
+
+    expect(filterFun({ time: 2018, geo: 'world' })).to.be.true;
+    expect(filterFun({ time: 2018, geo: 'country' })).to.be.true;
+    expect(filterFun({})).to.be.false;
+    expect(filterFun({ time: 2018, geo: 'nothing' })).to.be.false;
+    expect(filterFun({ time: 2018, geo: 'foo' })).to.be.true;
+
+    done();
+  });
+
+  it('not in operator', done => {
+    const sqlQuery = `
+    SELECT geo, time, income_mountains FROM datapoints 
+    WHERE time=2018 AND (geo NOT IN ('world', 'country') 
+    AND geo NOT IN ('foo')) ORDER BY time`;
+    const parser = new Parser();
+    const ast = parser.parse(sqlQuery);
+    const filterFun = getRecordFilterFun(sqlQuery, ast);
+
+    expect(filterFun({ time: 2018, geo: 'world' })).to.be.false;
+    expect(filterFun({ time: 2018, geo: 'country' })).to.be.false;
+    expect(filterFun({})).to.be.false;
+    expect(filterFun({ time: 2018, geo: 'nothing' })).to.be.true;
+    expect(filterFun({ time: 2018, geo: 'foo' })).to.be.false;
+
+    done();
+  });
+
+  it('not in and in operator', done => {
+    const sqlQuery = `
+    SELECT geo, time, income_mountains FROM datapoints 
+    WHERE time=2018 AND (geo IN ('world', 'country') 
+    AND geo NOT IN ('foo')) ORDER BY time`;
+    const parser = new Parser();
+    const ast = parser.parse(sqlQuery);
+    const filterFun = getRecordFilterFun(sqlQuery, ast);
+
+    expect(filterFun({ time: 2018, geo: 'world' })).to.be.true;
+    expect(filterFun({ time: 2018, geo: 'country' })).to.be.true;
+    expect(filterFun({})).to.be.false;
+    expect(filterFun({ time: 2018, geo: 'nothing' })).to.be.false;
+    expect(filterFun({ time: 2018, geo: 'foo' })).to.be.false;
 
     done();
   });
